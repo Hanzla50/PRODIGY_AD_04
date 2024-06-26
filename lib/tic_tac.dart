a092b0a44ba124bacfe410110db5a5484852d71b
Hanzla50 <hanzlajaved50281@gmail.com>
2024-06-26 23:12:37 +0500

    frot

--- lib/tic_tac.dart
+++ lib/tic_tac.dart
@@ -6,6 +6,10 @@ class ticTac extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('TICTACTOE GAME'),
+      ),
     );
   }
 }
+
